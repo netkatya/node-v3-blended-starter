23c9b816e293df9f36fa08f4fd3c174998003c5a
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-02 20:00:09 +0100

    model done

---
+++ src/models/product.js
@@ -0,0 +1,32 @@
+
+import { model, Schema } from "mongoose";
+
+const productSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        category: {
+            type: String,
+            enum: ['books', 'electronics', 'clothing', 'other'],
+            required: true,
+            default: 'other',
+        },
+        description: {
+            type: String,
+            required: false,
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+
+)
+
+export const Product = model('Product', productSchema);
