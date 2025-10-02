7240ce6315616b69a9bb8e44a456b719519056b6
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-02 20:29:40 +0100

    routes added

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,1 +1,13 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/products', getProducts);
+router.get('/products/:productId', getProductById);
+router.post('/products', createProduct);
+router.delete('/products/:productId', deleteProduct);
+router.patch('/products/:productId', updateNote);
+
+export default router;
+
 
