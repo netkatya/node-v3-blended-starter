ea221b24eadbe84478029a0144408e21303e9489
Andrii Butenko <andrejbutik@gmail.com>
2025-10-03 12:13:41 +0200

    Did Controllers

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
-
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from '../controllers/productsController.js';
 const router = Router();
 
 router.get('/products', getProducts);
 router.get('/products/:productId', getProductById);
 router.post('/products', createProduct);
 router.delete('/products/:productId', deleteProduct);
-router.patch('/products/:productId', updateNote);
+router.patch('/products/:productId', updateProduct);
 
 export default router;
-
-
