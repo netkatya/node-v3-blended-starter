c9b324aaabd868aa2251d9b485968a9ec760a502
Ievgen Prygoda <e.klimochenkova@gmail.com>
2025-10-05 13:31:44 +0100

    controllers updated

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,42 +1,47 @@
-import { Product } from '../models/product.js';
 import createHttpError from 'http-errors';
+import {
+createProductService,
+deleteProductService,
+getProductByIdService,
+getProductsService,
+updateProductService,
+} from '../services/productServices.js';
 
 export const getProducts = async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json(products);
+const products = await getProductsService();
+res.status(200).json(products);
 };
+
 export const getProductById = async (req, res, next) => {
-  const { productId } = req.params;
-  const product = await Product.findById(productId);
-  if (!product) {
-    next(createHttpError(404, 'Product not found'));
-    return;
-  }
-  res.status(200).json(product);
+const { productId } = req.params;
+const product = await getProductByIdService(productId);
+if (!product) {
+next(createHttpError(404, 'Product not found'));
+return;
+}
+res.status(200).json(product);
 };
 export const createProduct = async (req, res) => {
-  const product = await Product.create(req.body);
-  res.status(201).json(product);
+const product = await createProductService(req.body);
+res.status(201).json(product);
 };
+
 export const deleteProduct = async (req, res, next) => {
-  const { productId } = req.params;
-  const product = await Product.findOneAndDelete({
-    _id: productId,
-  });
-  if (!product) {
-    next(createHttpError(404, 'Product not found'));
-    return;
-  }
-  res.status(200).send(product);
+const { productId } = req.params;
+const product = await deleteProductService(productId);
+if (!product) {
+next(createHttpError(404, 'Product not found'));
+return;
+}
+res.status(200).send(product);
 };
+
 export const updateProduct = async (req, res, next) => {
-  const { productId } = req.params;
-  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
-    new: true,
-  });
-  if (!product) {
-    next(createHttpError(404, 'Product not found'));
-    return;
-  }
-  res.status(200).json(product);
+const { productId } = req.params;
+const product = await updateProductService({ productId, body: req.body });
+if (!product) {
+next(createHttpError(404, 'Product not found'));
+return;
+}
+res.status(200).json(product);
 };
