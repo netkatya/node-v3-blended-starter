a6d6f5f8533dbcbb77c3dbb7e9176c98fa69324c
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-10-15 23:11:28 +0300

    pagination, filter, sort

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import { Product } from '../models/product.js';
 
 export const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGO_URL;
     await mongoose.connect(mongoUrl);
     console.log('✅ MongoDB connection established successfully');
+    await Product.syncIndexes(); // Ensure indexes are created
   } catch (error) {
     console.error('❌ Failed to connect to MongoDB:', error.message);
     process.exit(1);
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 
 import {
   getProducts,
@@ -7,12 +8,18 @@ import {
   deleteProduct,
   updateProduct,
 } from '../controllers/productsController.js';
+import {
+  createProductSchema,
+  getProductsSchema,
+  productIdSchema,
+  updateProductSchema,
+} from '../validation/productsValidation.js';
 const router = Router();
 
-router.get('/', getProducts);
-router.get('/:productId', getProductById);
-router.post('/', createProduct);
-router.delete('/:productId', deleteProduct);
-router.patch('/:productId', updateProduct);
+router.get('/', celebrate(getProductsSchema), getProducts);
+router.get('/:productId', celebrate(productIdSchema), getProductById);
+router.post('/', celebrate(createProductSchema), createProduct);
+router.delete('/:productId', celebrate(productIdSchema), deleteProduct);
+router.patch('/:productId', celebrate(updateProductSchema), updateProduct);
 
 export default router;
