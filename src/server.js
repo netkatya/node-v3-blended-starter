b662ac84b710461d4ee0029f0ef3575587cd32c7
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-10-26 21:38:57 +0200

    avatar

--- src/server.js
+++ src/server.js
@@ -2,15 +2,17 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import helmet from "helmet";
-
 import {errors} from 'celebrate';
+import cookieParser from "cookie-parser";
+
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+
 import productsRouter from './routes/productsRoutes.js';
 import authRoutes from './routes/authRoutes.js';
-import cookieParser from "cookie-parser";
+import userRoutes from './routes/userRoutes.js'
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -22,9 +24,10 @@ app.use(helmet())
 app.use(cookieParser());
 
 app.use('/products', productsRouter);
-app.use(authRoutes)
+app.use(authRoutes);
+app.use(userRoutes)
 
-// heandle 404
+// handle 404
 app.use(notFoundHandler);
 
 // handle celebrate errors
